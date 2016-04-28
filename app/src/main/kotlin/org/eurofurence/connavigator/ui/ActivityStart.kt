d113c517c6baf6195644415b771f4b07e10d0391
Requinard <contact@terarion.com>
2016-04-28 14:03:00 +0200

    Set up a start screen to have users wait while the application loads some data. Will alter be implemented to have a quick tutorial and stuff

--- app/src/main/kotlin/org/eurofurence/connavigator/ui/ActivityStart.kt
+++ app/src/main/kotlin/org/eurofurence/connavigator/ui/ActivityStart.kt
@@ -2,23 +2,36 @@ package org.eurofurence.connavigator.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import org.eurofurence.connavigator.R
 import org.eurofurence.connavigator.database.Database
+import org.eurofurence.connavigator.database.UpdateIntentService
 import org.eurofurence.connavigator.util.delegators.view
+import org.eurofurence.connavigator.util.extensions.*
+import java.util.*
 
 /**
  * Created by David on 28-4-2016.
  */
 class ActivityStart : AppCompatActivity() {
     val buttonStart by view(Button::class.java)
+    val textHelp by view(TextView::class.java)
+
+    val updateReceiver = localReceiver(UpdateIntentService.UPDATE_COMPLETE) {
+        textHelp.text = "There, all done!"
+        buttonStart.visibility = View.VISIBLE
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_start)
+
+        updateReceiver.register()
 
         val db = Database(this)
 
@@ -28,8 +41,16 @@ class ActivityStart : AppCompatActivity() {
         buttonStart.setOnClickListener {
             startRootActivity()
         }
+    }
 
-        buttonStart.visibility = View.VISIBLE
+    override fun onResume() {
+        super.onResume()
+        updateReceiver.register()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        updateReceiver.unregister()
     }
 
     private fun startRootActivity() {
