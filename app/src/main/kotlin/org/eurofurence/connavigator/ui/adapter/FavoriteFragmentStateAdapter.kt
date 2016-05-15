7825f83ad996f5c657d3767de066c1c27b9ea672
David Diks <d.diks94@gmail.com>
2016-05-15 11:46:21 +0200

    Home screen now sorts favorited by date first, then time

--- app/src/main/kotlin/org/eurofurence/connavigator/ui/adapter/FavoriteFragmentStateAdapter.kt
+++ app/src/main/kotlin/org/eurofurence/connavigator/ui/adapter/FavoriteFragmentStateAdapter.kt
@@ -5,13 +5,14 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
 import org.eurofurence.connavigator.database.Database
 import org.eurofurence.connavigator.ui.fragments.EventCard
+import org.eurofurence.connavigator.util.extensions.get
 
 /**
  * Created by David on 5/14/2016.
  */
 class FavoriteFragmentStateAdapter(val fragmentManager: FragmentManager, val database: Database) : FragmentStatePagerAdapter(fragmentManager) {
     override fun getItem(position: Int): Fragment? {
-        return EventCard(database.favoritedDb.items.elementAt(position))
+        return EventCard(database.favoritedDb.asc { database.eventConferenceDayDb[it.conferenceDayId]?.date + it.startTime }.elementAt(position))
     }
 
     override fun getCount(): Int {
