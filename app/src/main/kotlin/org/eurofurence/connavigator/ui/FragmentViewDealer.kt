58a533b10c538211f81657459f3db87303ff418a
Requinard <contact@terarion.com>
2016-05-16 14:03:00 +0200

    Clicking on a dealer now opens a dealer's detail screen

--- app/src/main/kotlin/org/eurofurence/connavigator/ui/FragmentViewDealer.kt
+++ app/src/main/kotlin/org/eurofurence/connavigator/ui/FragmentViewDealer.kt
@@ -1,10 +1,28 @@
 package org.eurofurence.connavigator.ui
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import io.swagger.client.model.Dealer
+import org.eurofurence.connavigator.R
+import org.eurofurence.connavigator.tracking.Analytics
+import org.eurofurence.connavigator.util.delegators.view
 
 /**
  * Created by David on 16-5-2016.
  */
-class FragmentViewDealer(val dealer: Dealer): Fragment() {
+class FragmentViewDealer(val dealer: Dealer) : Fragment() {
+    val dealerName by view(TextView::class.java)
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
+            inflater.inflate(R.layout.fview_dealer, container, false)
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        Analytics.changeScreenName("View Dealer Details")
+
+        dealerName.text = dealer.attendeeNickname
+    }
 }
