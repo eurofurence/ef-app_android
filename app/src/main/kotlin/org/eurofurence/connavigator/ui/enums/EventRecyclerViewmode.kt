8ed1e4a624763ee05b7f2bde109054e3dcedda02
Requinard <contact@terarion.com>
2016-05-26 00:03:08 +0200

    Added event recycler viewmode

---
+++ app/src/main/kotlin/org/eurofurence/connavigator/ui/enums/EventRecyclerViewmode.kt
@@ -0,0 +1,13 @@
+package org.eurofurence.connavigator.ui.enums
+
+/**
+ * Created by David on 25-5-2016.
+ */
+enum class EventRecyclerViewmode {
+    ALL, // All events
+    DAY, // Events happening on a specific day
+    FAVORITED, // Events that have been favourited
+    FAVORITED_DAY, // Events that have been favourited, ordered by day
+    CURRENT, // Events that are happening right now
+    UPCOMING, // Events that are going to happen in 30 minutes
+}
