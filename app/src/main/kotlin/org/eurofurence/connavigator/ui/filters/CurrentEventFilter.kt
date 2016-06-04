edfe7de4d3eaca96418d5f0c4199152041101373
Lukas Härtel <lukashaertel@googlemail.com>
2016-06-04 17:39:33 +0200

    Current events filter reverse containment

--- app/src/main/kotlin/org/eurofurence/connavigator/ui/filters/CurrentEventFilter.kt
+++ app/src/main/kotlin/org/eurofurence/connavigator/ui/filters/CurrentEventFilter.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import io.swagger.client.model.EventEntry
 import org.eurofurence.connavigator.database.Database
 import org.joda.time.DateTime
-import org.joda.time.Interval
 
 /**
  * Created by David on 6/4/2016.
@@ -16,11 +15,9 @@ class CurrentEventFilter : IEventFilter {
         val database = Database(context)
         val nowDate = now.toString("yyyy-MM-dd")
 
-        val nextThirty = Interval(now, now.plusMinutes(30))
-
         // Get today, otherwise first day
         val closestEventDay = database.eventConferenceDayDb.items.find { it.date == nowDate } ?: database.eventConferenceDayDb.asc { it.date }.first()
 
-        return database.eventEntryDb.items.filter { it.conferenceDayId == closestEventDay.id && nextThirty.overlaps(database.eventInterval(it)) }.sortedBy { it.startTime }
+        return database.eventEntryDb.items.filter { it.conferenceDayId == closestEventDay.id && database.eventInterval(it).contains(now) }.sortedBy { it.startTime }
     }
 }
