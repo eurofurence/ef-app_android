db9c22265d01111c08d1638c8aaaa88a26efa7ea
requinard <d.diks94@gmail.com>
2016-06-05 09:46:42 +0200

    Event recyclers now dissapear if they have no contents

--- app/src/main/kotlin/org/eurofurence/connavigator/ui/fragments/EventRecyclerFragment.kt
+++ app/src/main/kotlin/org/eurofurence/connavigator/ui/fragments/EventRecyclerFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.CardView
 import android.support.v7.widget.DefaultItemAnimator
-import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -103,6 +102,11 @@ class EventRecyclerFragment(val filterStrategy: IEventFilter, val filterVal: Any
         }
 
         updateReceiver.register()
+
+        if (effectiveEvents.isEmpty()) {
+            eventsTitle.visibility = View.GONE
+            events.visibility = View.GONE
+        }
     }
 
     override fun onPause() {
