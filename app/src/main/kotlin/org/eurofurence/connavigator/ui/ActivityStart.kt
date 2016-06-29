f538369f2170889908d7c83c927f79abdc83dbcc
requinard <d.diks94@gmail.com>
2016-06-29 16:44:10 +0200

    We're now preloading all maps

--- app/src/main/kotlin/org/eurofurence/connavigator/ui/ActivityStart.kt
+++ app/src/main/kotlin/org/eurofurence/connavigator/ui/ActivityStart.kt
@@ -6,12 +6,18 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener
 import org.eurofurence.connavigator.R
 import org.eurofurence.connavigator.database.Database
 import org.eurofurence.connavigator.database.UpdateIntentService
+import org.eurofurence.connavigator.net.imageService
+import org.eurofurence.connavigator.tracking.Analytics
 import org.eurofurence.connavigator.util.delegators.view
 import org.eurofurence.connavigator.util.extensions.booleans
+import org.eurofurence.connavigator.util.extensions.get
 import org.eurofurence.connavigator.util.extensions.localReceiver
+import org.eurofurence.connavigator.util.extensions.logd
+import org.eurofurence.connavigator.webapi.apiService
 
 /**
  * Created by David on 28-4-2016.
@@ -22,12 +28,20 @@ class ActivityStart : AppCompatActivity() {
 
     val updateReceiver = localReceiver(UpdateIntentService.UPDATE_COMPLETE) {
         if (it.booleans["success"]) {
+            val database = Database(this)
+
+            for (map in database.mapEntityDb.items) {
+                logd { "Preloading map ${map.description}" }
+                val image = database.imageDb[map.imageId]!!
+                imageService.imageLoader.loadImage(apiService.formatUrl(image.url), SimpleImageLoadingListener())
+            }
+
             textHelp.text = "There, all done!"
             buttonStart.visibility = View.VISIBLE
 
             buttonStart.setOnClickListener { startRootActivity() }
         } else {
-            textHelp.text = "Failed to successfully get data from the backend. Press the button to try again"
+            textHelp.text = "Failed to successfully get data. Are you connected to the internet?"
             buttonStart.visibility = View.VISIBLE
             buttonStart.setOnClickListener { UpdateIntentService.dispatchUpdate(this) }
         }
@@ -37,6 +51,8 @@ class ActivityStart : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        Analytics.screen("Start")
 
         // Data is present, if a database has a backing file
         if (database.eventConferenceDayDb.time != null)
