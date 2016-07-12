cfbd87c4637ccaf2bcb0787d47e34d46f7e191fd
requinard <d.diks94@gmail.com>
2016-07-12 13:58:56 +0200

    Home now get updated when new data is present

--- app/src/main/kotlin/org/eurofurence/connavigator/ui/FragmentViewHome.kt
+++ app/src/main/kotlin/org/eurofurence/connavigator/ui/FragmentViewHome.kt
@@ -12,20 +12,23 @@ import org.eurofurence.connavigator.R
 import org.eurofurence.connavigator.database.Database
 import org.eurofurence.connavigator.tracking.Analytics
 import org.eurofurence.connavigator.ui.adapter.AnnoucementRecyclerDataAdapter
+import org.eurofurence.connavigator.ui.communication.ContentAPI
 import org.eurofurence.connavigator.ui.filters.enums.EnumEventRecyclerViewmode
 import org.eurofurence.connavigator.ui.filters.factory.EventFilterFactory
 import org.eurofurence.connavigator.ui.fragments.EventRecyclerFragment
 import org.eurofurence.connavigator.ui.layouts.NonScrollingLinearLayout
 import org.eurofurence.connavigator.util.delegators.view
 import org.eurofurence.connavigator.util.extensions.applyOnRoot
+import org.eurofurence.connavigator.util.extensions.letRoot
 import org.eurofurence.connavigator.util.extensions.size
 
 /**
  * Created by David on 5/14/2016.
  */
-class FragmentViewHome : Fragment() {
+class FragmentViewHome : Fragment(), ContentAPI {
     val announcementsRecycler by view(RecyclerView::class.java)
     val announcementsTitle by view(TextView::class.java)
+    val database: Database by lazy { letRoot { database }!! }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
             inflater.inflate(R.layout.fview_home, container, false)
@@ -33,9 +36,16 @@ class FragmentViewHome : Fragment() {
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         Analytics.screen("Home")
 
-        val database = Database(activity)
         applyOnRoot { changeTitle("Home") }
 
+        instantiate()
+    }
+
+    override fun dataUpdated() {
+        instantiate()
+    }
+
+    private fun instantiate() {
         announcementsRecycler.adapter = AnnoucementRecyclerDataAdapter(database.announcementDb.items.sortedByDescending { it.lastChangeDateTimeUtc }.toList())
         announcementsRecycler.layoutManager = NonScrollingLinearLayout(activity)
         announcementsRecycler.itemAnimator = DefaultItemAnimator()
