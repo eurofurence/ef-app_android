b54dd5ebfd54dcc2db12d1c97655224393dc960c
requinard <d.diks94@gmail.com>
2017-06-20 14:43:30 +0200

    feat(analytics): move analytics to firebase
    We now use firebase instead of google analytics

--- app/src/main/kotlin/org/eurofurence/connavigator/tracking/Analytics.kt
+++ app/src/main/kotlin/org/eurofurence/connavigator/tracking/Analytics.kt
@@ -12,6 +12,7 @@ import org.eurofurence.connavigator.R
 import org.eurofurence.connavigator.util.extensions.limit
 import org.eurofurence.connavigator.util.extensions.logd
 import org.eurofurence.connavigator.util.extensions.logv
+import org.jetbrains.anko.bundleOf
 
 /**
  * Created by David on 20-4-2016.
@@ -43,110 +44,30 @@ class Analytics {
     }
 
     companion object {
-        val LOGTAG = "ANAL"
-        lateinit var tracker: Tracker
-        lateinit var context: Context
-        lateinit var firebaseAnalytics: FirebaseAnalytics
-
-        fun init(context: Context) {
-            logd { "Initializing Google Analytics Tracking" }
-
-            // Get shared preferences
-            val preferences = PreferenceManager.getDefaultSharedPreferences(context)!!
-
-            this.context = context
-
-
-            // Get the new tracking
-            updateTracking(preferences)
+        lateinit var analytics: FirebaseAnalytics
+        fun init(context: Context){
+            analytics = FirebaseAnalytics.getInstance(context)
         }
 
-        fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
-            if (key.contains("analytics")) {
-                logd { "Analytics settings have been updated" }
-                updateTracking(sharedPreferences)
-
-                // Get firebase analytics
-                firebaseAnalytics = FirebaseAnalytics.getInstance(context)
-
-            }
-        }
-
-        /**
-         * Update the tracking settings
-         */
-        private fun updateTracking(preferences: SharedPreferences) {
-            logv { "Updating tracking to new stats" }
-
-            // Set app-level opt out
-            val analytics_on = preferences.getBoolean(context.resources.getString(R.string.settings_tag_analytics_enabled), true)
-
-            GoogleAnalytics.getInstance(context).appOptOut = analytics_on
-
-            // Start tracking
-
-            // Set debug or production version
-            if (BuildConfig.DEBUG) {
-                tracker = GoogleAnalytics.getInstance(context).newTracker("UA-76443357-2")
-            } else {
-                tracker = GoogleAnalytics.getInstance(context).newTracker("UA-76443357-1")
-            }
-
-            // Set sampling rate
-            tracker.setSampleRate(100.0)
-
-            //Track exceptions
-            tracker.enableExceptionReporting(true)
-
-            // Anonymize IP
-            tracker.setAnonymizeIp(true)
-        }
 
         /**
          * Change screen and report
          */
-        fun screen(screenName: String) {
-            tracker.setScreenName(screenName)
-            tracker.send(HitBuilders.ScreenViewBuilder().build())
-        }
+        fun screen(screenName: String) = analytics.logEvent(
+                FirebaseAnalytics.Event.VIEW_ITEM,
+                bundleOf(FirebaseAnalytics.Param.ITEM_NAME to screenName,
+                        FirebaseAnalytics.Param.CONTENT_TYPE to "screen")
+        )
 
-        /**
-         * Send an event to analytics
-         */
-        fun event(eventBuilder: HitBuilders.EventBuilder) =
-                tracker.send(eventBuilder.build())
+        fun event(category: String, action: String, label: String) = analytics.logEvent(
+                FirebaseAnalytics.Event.SELECT_CONTENT,
+                bundleOf(
+                        FirebaseAnalytics.Param.CONTENT_TYPE to category,
+                        FirebaseAnalytics.Param.ITEM_CATEGORY to action,
+                        FirebaseAnalytics.Param.ITEM_NAME to label
+                )
+        )
 
-        /**
-         * Send an event to analytics using predefined statuses
-         */
-        fun event(category: String, action: String, label: String) {
-            event(HitBuilders.EventBuilder()
-                    .setCategory(category)
-                    .setAction(action)
-                    .setLabel(label)) }
-
-        /**
-         * Track an exception
-         */
-        fun exception(description: String, fatal: Boolean = false) =
-                tracker.send(HitBuilders.ExceptionBuilder()
-                        .setDescription(description)
-                        .setFatal(fatal)
-                        .build())
-
-        /**
-         * Tracks a non-fatal exception, getting it's stacktrace from context
-         */
-        fun exception(ex: Throwable) {
-            // Get the stats regarding the method outside
-            val stackTrace = Thread.currentThread().stackTrace[2]
-            val mess = "${stackTrace.className}.${stackTrace.methodName}:${stackTrace.lineNumber} ${ex.javaClass.simpleName}"
-
-            if (mess.length >= 100) {
-                exception(mess.substring(0, 100))
-            } else {
-                exception(mess)
-            }
-        }
+        fun exception(ex: Throwable) = Unit
     }
 }
