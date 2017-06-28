951debf44710c61fae10f0761f81684ce9ca620e
requinard <d.diks94@gmail.com>
2017-06-28 21:40:43 +0200

    feat(ui): refactor dealerView to anko

--- app/src/main/kotlin/org/eurofurence/connavigator/ui/FragmentViewDealer.kt
+++ app/src/main/kotlin/org/eurofurence/connavigator/ui/FragmentViewDealer.kt
@@ -1,60 +1,47 @@
 package org.eurofurence.connavigator.ui
 
-import android.graphics.Bitmap
-import android.graphics.Matrix
+import android.os.Build
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
-import com.google.firebase.perf.metrics.AddTrace
 import io.swagger.client.model.DealerRecord
-import io.swagger.client.model.ImageRecord
-import io.swagger.client.model.MapEntryRecord
 import org.eurofurence.connavigator.R
 import org.eurofurence.connavigator.database.HasDb
 import org.eurofurence.connavigator.database.lazyLocateDb
 import org.eurofurence.connavigator.net.imageService
-import org.eurofurence.connavigator.pref.RemotePreferences
 import org.eurofurence.connavigator.tracking.Analytics
 import org.eurofurence.connavigator.ui.communication.ContentAPI
 import org.eurofurence.connavigator.util.Formatter
-import org.eurofurence.connavigator.util.delegators.view
-import org.eurofurence.connavigator.util.extensions.*
+import org.eurofurence.connavigator.util.extensions.contains
+import org.eurofurence.connavigator.util.extensions.jsonObjects
+import org.eurofurence.connavigator.util.extensions.markdownView
+import org.eurofurence.connavigator.util.extensions.photoView
 import org.eurofurence.connavigator.util.v2.get
+import org.jetbrains.anko.*
+import uk.co.senab.photoview.PhotoView
 import us.feras.mdv.MarkdownView
 
 /**
  * Created by David on 16-5-2016.
  */
-class FragmentViewDealer() : Fragment(), ContentAPI, HasDb {
+class FragmentViewDealer() : Fragment(), ContentAPI, HasDb, AnkoLogger {
     constructor(dealer: DealerRecord) : this() {
         arguments = Bundle()
 
         arguments.jsonObjects["dealer"] = dealer
     }
 
+    val ui by lazy { DealerUi() }
+
     override val db by lazyLocateDb()
 
-    val dealerName: TextView by view()
-    val dealerShortDescription: TextView by view()
-    val dealerArtistDescription: MarkdownView by view()
-    val dealerArtDescription: MarkdownView by view()
-    val dealerImage: ImageView by view()
-    val dealerButtonMore: FloatingActionButton by view()
-    val dealerPreviewArtImage: ImageView by view()
-    val dealerPreviewCaption: TextView by view()
-    val dealerMap: ImageView by view()
-
-    val dealerPreviewArtLayout: LinearLayout by view()
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
-            inflater.inflate(R.layout.fview_dealer, container, false)
+            ui.createView(AnkoContext.Companion.create(container!!.context, container))
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         // Send analytics pings
@@ -68,110 +55,123 @@ class FragmentViewDealer() : Fragment(), ContentAPI, HasDb {
             // Retrieve top image
             val image = dealer[toArtistImage]
 
-            // Load the map
-
-            /* TODO
-            val mapEntry = database.mapEntryDb.items.find { it.targetId == dealer.id }
-
-            val mapImage = database.imageDb[database.mapEntityDb[mapEntry?.mapId]?.imageId]
-
-            resizeMap(mapEntry, mapImage)
-            */
-
             // Set image on top
             if (image != null) {
-                imageService.load(image, dealerImage, false)
+                imageService.load(image, ui.primaryImage, false)
             } else {
-                dealerImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.dealer_white_full))
+                ui.primaryImage.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.dealer_white_full))
             }
 
             // Load art preview image
-            imageService.load(dealer[toPreview], dealerPreviewArtImage)
+            imageService.load(dealer[toPreview], ui.artPreview)
 
-            if (dealerPreviewArtImage.visibility == View.GONE) {
-                dealerPreviewArtLayout.visibility = View.GONE
-            }
+            ui.artPreviewCaption.text = dealer.artPreviewCaption
 
-            dealerPreviewCaption.text = dealer.artPreviewCaption
+            ui.name.text = Formatter.dealerName(dealer)
+            ui.shortDescription.text = dealer.shortDescription
 
-            dealerName.text = Formatter.dealerName(dealer)
-            dealerShortDescription.text = dealer.shortDescription
+            ui.aboutArtist.loadMarkdown(
+                    if (dealer.aboutTheArtistText.isNotEmpty())
+                        dealer.aboutTheArtistText
+                    else
+                        "This artist did not supply any artist description to show to you :("
+            )
 
-            dealerArtistDescription.loadMarkdown(dealer.aboutTheArtistText)
-            dealerArtDescription.loadMarkdown(dealer.aboutTheArtText)
+            ui.aboutArt.loadMarkdown(
+                    if (dealer.aboutTheArtText.isNotEmpty())
+                        dealer.aboutTheArtText
+                    else
+                        "this artist did not supply any art descriptions to show to you :("
+            )
+        }
+    }
+}
 
-            // Handle the FAB that links out
-            dealerButtonMore.setOnClickListener {
-                {
-                    /* TODO
-                    if (dealer.websiteUri.startsWith("http")) {
-                        Analytics.event(Analytics.Category.DEALER, Analytics.Action.LINK_CLICKED, dealer.displayName ?: dealer.attendeeNickname)
+class DealerUi : AnkoComponent<ViewGroup> {
+    lateinit var primaryImage: PhotoView
+    lateinit var name: TextView
+    lateinit var shortDescription: TextView
+    lateinit var aboutArtist: MarkdownView
+    lateinit var aboutArt: MarkdownView
+    lateinit var artPreview: PhotoView
+    lateinit var artPreviewCaption: TextView
+    lateinit var map: PhotoView
 
-                        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(dealer.websiteUri)))
-                    } else {
-                        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://" + dealer.websiteUri)))
+    override fun createView(ui: AnkoContext<ViewGroup>) = with(ui) {
+        relativeLayout {
+            backgroundResource = R.color.cardview_light_background
+            scrollView {
+                verticalLayout {
+                    lparams(matchParent, matchParent)
+
+                    primaryImage = photoView {
+                        lparams(matchParent, dip(300))
+                        backgroundResource = R.drawable.image_fade
                     }
-                    */
-                } catchAlternative { _: Exception ->
-                    logv { "User tried clicking on a dealer with no url" }
+
+                    verticalLayout {
+                        lparams(matchParent, wrapContent)
+                        backgroundResource = R.color.primaryDarker
+                        padding = dip(15)
+
+                        name = textView {
+                            text = "Dealer Name"
+                            textAlignment = View.TEXT_ALIGNMENT_CENTER
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                setTextAppearance(android.R.style.TextAppearance_Large_Inverse)
+                            }
+                            padding = dip(10)
+                        }
+
+                        shortDescription = textView {
+                            text = "Short description"
+                            padding = dip(10)
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                setTextAppearance(android.R.style.TextAppearance_Medium_Inverse)
+                            }
+                        }
+                    }
+
+                    textView {
+                        text = "About the Artist"
+                        padding = dip(15)
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            setTextAppearance(R.style.TextAppearance_AppCompat_Large)
+                        }
+                    }
+
+                    aboutArtist = markdownView { }
+
+                    artPreview = photoView {
+                        lparams(matchParent, dip(400))
+                        scaleType = ImageView.ScaleType.FIT_CENTER
+                        backgroundResource = R.color.primary
+                    }
+
+                    artPreviewCaption = textView {
+                        textAlignment = View.TEXT_ALIGNMENT_CENTER
+                        padding = dip(15)
+                        backgroundResource = R.color.primary
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            setTextAppearance(R.style.TextAppearance_AppCompat_Medium_Inverse)
+                        }
+                    }
+
+                    textView("About the Art") {
+                        padding = dip(15)
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            setTextAppearance(R.style.TextAppearance_AppCompat_Large)
+                        }
+                    }
+                    aboutArt = markdownView { }
+
+                    map = photoView {
+                        lparams(matchParent, dip(200))
+                        scaleType = ImageView.ScaleType.FIT_CENTER
+                        imageResource = R.drawable.placeholder_event
+                    }
                 }
             }
-
-            // Load empty texts
-            /* TODO
-            if (dealer.websiteUri.isEmpty())
-                dealerButtonMore.visibility = View.GONE
-            */
-
-            if (dealer.aboutTheArtText.isEmpty())
-                dealerArtistDescription.loadMarkdown("This artist did not supply any artist description to show to you :(")
-
-            if (dealer.aboutTheArtistText.isEmpty())
-                dealerArtDescription.loadMarkdown("this artist did not supply any art descriptions to show to you :(")
-
-            if (dealer.shortDescription.isEmpty())
-                dealerShortDescription.visibility = View.GONE
         }
     }
-
-    @AddTrace(name = "FragmentViewDealer:resizeMap", enabled = true)
-    private fun resizeMap(mapEntry: MapEntryRecord?, mapImage: ImageRecord?) {
-        if (mapEntry == null || mapImage == null) {
-            dealerMap.visibility = View.GONE
-            return
-        }
-
-
-        val bitmap = imageService.getBitmap(mapImage)
-
-
-        val dealerCoords = mapEntry.asRelatedCoordinates(mapImage)
-
-        var width = RemotePreferences.dealerMapWidth.toInt()
-        var height = RemotePreferences.dealerMapHeight.toInt()
-
-        val matrix = Matrix()
-
-        // set coordinates
-        var x = dealerCoords.x
-        var y = dealerCoords.y
-
-        // Move X and y higher and lefter to to make coords we have the middle
-        x -= width / 2
-        y -= height / 2
-
-        // Fix if less then 0
-        if (x < 0) x = 0
-        if (y < 0) y = 0
-
-        // Fix image bounds
-        if (x + width > bitmap.width) width = bitmap.width - x
-        if (y + height > bitmap.height) height = bitmap.height - y
-
-        //crop
-        val croppedMap = Bitmap.createBitmap(bitmap, x, y, width, height, matrix, true)
-
-        dealerMap.setImageBitmap(croppedMap)
-    }
-
 }
--- app/src/main/kotlin/org/eurofurence/connavigator/util/extensions/AnkoExtensions.kt
+++ app/src/main/kotlin/org/eurofurence/connavigator/util/extensions/AnkoExtensions.kt
@@ -1,14 +1,18 @@
 package org.eurofurence.connavigator.util.extensions
 
 import android.support.v7.widget.RecyclerView
-import android.view.View
 import android.view.ViewManager
 import com.github.lzyzsd.circleprogress.ArcProgress
 import org.jetbrains.anko.custom.ankoView
+import uk.co.senab.photoview.PhotoView
+import us.feras.mdv.MarkdownView
 
 /**
  * Extensions to anko viewmanager
  */
 
 public inline fun ViewManager.arcProgress(init: ArcProgress.() -> Unit) = ankoView({ ArcProgress(it) }, 0, init)
+
 public inline fun ViewManager.recycler(init: RecyclerView.() -> Unit) = ankoView({ RecyclerView(it) }, 0, init)
+public inline fun ViewManager.photoView(init: PhotoView.() -> Unit) = ankoView({ PhotoView(it) }, 0, init)
+inline fun ViewManager.markdownView(init: MarkdownView.() -> Unit) = ankoView({MarkdownView(it)}, 0, init)
