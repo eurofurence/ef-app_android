1af8262739e9a11dcfb053f1056ef9cfb1750d72
Luchs <cougarism@gmail.com>
2017-07-06 16:27:04 +0200

    style(fragmentViewDealer): prettify dealer screen & visibility states

--- app/src/main/kotlin/org/eurofurence/connavigator/ui/FragmentViewDealer.kt
+++ app/src/main/kotlin/org/eurofurence/connavigator/ui/FragmentViewDealer.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.github.chrisbanes.photoview.PhotoView
 import io.swagger.client.model.DealerRecord
@@ -17,10 +18,7 @@ import org.eurofurence.connavigator.net.imageService
 import org.eurofurence.connavigator.tracking.Analytics
 import org.eurofurence.connavigator.ui.communication.ContentAPI
 import org.eurofurence.connavigator.util.Formatter
-import org.eurofurence.connavigator.util.extensions.contains
-import org.eurofurence.connavigator.util.extensions.jsonObjects
-import org.eurofurence.connavigator.util.extensions.markdownView
-import org.eurofurence.connavigator.util.extensions.photoView
+import org.eurofurence.connavigator.util.extensions.*
 import org.eurofurence.connavigator.util.v2.get
 import org.jetbrains.anko.*
 import us.feras.mdv.MarkdownView
@@ -76,12 +74,18 @@ class FragmentViewDealer() : Fragment(), ContentAPI, HasDb, AnkoLogger {
                         "This artist did not supply any artist description to show to you :("
             )
 
-            ui.aboutArt.loadMarkdown(
-                    if (dealer.aboutTheArtText.isNotEmpty())
-                        dealer.aboutTheArtText
-                    else
-                        "this artist did not supply any art descriptions to show to you :("
-            )
+            if (dealer.artPreviewImageId == null) {
+                ui.artPreview.visibility = View.GONE
+
+                if (dealer.artPreviewCaption.isEmpty()) {
+                    ui.artPreviewContainer .visibility = View.GONE
+                }
+            }
+
+            if (dealer.aboutTheArtText.isNotEmpty())
+                ui.aboutArt.loadMarkdown(dealer.aboutTheArtText)
+            else
+                ui.aboutArtContainer.visibility = View.GONE
 
             ui.map.setScale(2.5F, true)
         }
@@ -94,8 +98,10 @@ class DealerUi : AnkoComponent<ViewGroup> {
     lateinit var shortDescription: TextView
     lateinit var aboutArtist: MarkdownView
     lateinit var aboutArt: MarkdownView
+    lateinit var aboutArtContainer: LinearLayout
     lateinit var artPreview: PhotoView
     lateinit var artPreviewCaption: TextView
+    lateinit var artPreviewContainer: LinearLayout
     lateinit var map: PhotoView
 
     override fun createView(ui: AnkoContext<ViewGroup>) = with(ui) {
@@ -128,38 +134,50 @@ class DealerUi : AnkoComponent<ViewGroup> {
                             padding = dip(10)
 
                             setTextAppearance(ctx, android.R.style.TextAppearance_Medium_Inverse)
-
                         }
                     }
 
                     textView {
                         text = "About the Artist"
-                        padding = dip(15)
-                        setTextAppearance(ctx, R.style.TextAppearance_AppCompat_Large)
-
-                    }
-
-                    aboutArtist = markdownView { }
-
-                    artPreview = photoView {
-                        lparams(matchParent, dip(400))
-                        scaleType = ImageView.ScaleType.FIT_CENTER
-                        backgroundResource = R.color.primary
-                    }
-
-                    artPreviewCaption = textView {
-                        textAlignment = View.TEXT_ALIGNMENT_CENTER
-                        padding = dip(15)
-                        backgroundResource = R.color.primary
-                        setTextAppearance(ctx, R.style.TextAppearance_AppCompat_Medium_Inverse)
-
-                    }
-
-                    textView("About the Art") {
-                        padding = dip(15)
+                        setPadding(dip(25), dip(20), dip(25), dip(0))
                         setTextAppearance(ctx, R.style.TextAppearance_AppCompat_Large)
                     }
-                    aboutArt = markdownView { }
+
+                    aboutArtist = markdownView {
+                        lparams {
+                            margin = dip(15)
+                        }
+                    }
+
+                    artPreviewContainer = verticalLayout {
+                        padding = dip(15)
+                        backgroundResource = R.color.primary
+
+                        artPreview = photoView {
+                            lparams(matchParent, dip(400))
+                            scaleType = ImageView.ScaleType.FIT_CENTER
+                        }
+
+                        artPreviewCaption = textView {
+                            textAlignment = View.TEXT_ALIGNMENT_CENTER
+                            padding = dip(15)
+                            setTextAppearance(ctx, R.style.TextAppearance_AppCompat_Medium_Inverse)
+                        }
+                    }
+
+                    aboutArtContainer = verticalLayout {
+                        lparams(matchParent, wrapContent)
+
+                        textView("About the Art") {
+                            setPadding(dip(25), dip(20), dip(25), dip(0))
+                            setTextAppearance(ctx, R.style.TextAppearance_AppCompat_Large)
+                        }
+                        aboutArt = markdownView {
+                            lparams {
+                                margin = dip(15)
+                            }
+                        }
+                    }
 
                     map = photoView {
                         lparams(matchParent, dip(200))
