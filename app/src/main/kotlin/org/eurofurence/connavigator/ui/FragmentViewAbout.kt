d09b53308dea678a323364bdeb122d2c9232f01b
Luchs <luchs@pinselohr.ch>
2018-07-27 15:06:14 +0200

    ui(about): adding disclaimer

--- app/src/main/kotlin/org/eurofurence/connavigator/ui/FragmentViewAbout.kt
+++ app/src/main/kotlin/org/eurofurence/connavigator/ui/FragmentViewAbout.kt
@@ -153,7 +153,7 @@ class AboutUi : AnkoComponent<ViewGroup> {
                     markdownView {
                         lparams(matchParent, wrapContent)
 
-                        loadMarkdown("""## Also thanks to
+                        loadMarkdown("""
 **iOS:**
 
 - Fenrikur
@@ -188,6 +188,11 @@ class AboutUi : AnkoComponent<ViewGroup> {
 - FontAwesome
 - Kotlin
 - Anko
+
+
+**Disclaimer**
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
                     """)
                     }
                 }.lparams(matchParent, wrapContent) {
