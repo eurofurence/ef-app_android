bfbe8b227cc855b8087390c2a7da43686bf3a958
requinard <d.diks94@gmail.com>
2018-07-27 14:06:17 +0200

    refactor(about): implement new about design
    implement new design and move support chat button to remote config

--- app/src/main/kotlin/org/eurofurence/connavigator/ui/FragmentViewAbout.kt
+++ app/src/main/kotlin/org/eurofurence/connavigator/ui/FragmentViewAbout.kt
@@ -17,8 +17,22 @@ import org.eurofurence.connavigator.ui.communication.ContentAPI
 import org.eurofurence.connavigator.util.delegators.view
 import org.eurofurence.connavigator.util.extensions.applyOnRoot
 import org.eurofurence.connavigator.util.extensions.markdownView
-import org.jetbrains.anko.*
+import org.eurofurence.connavigator.util.extensions.now
+import org.eurofurence.connavigator.util.v2.compatAppearance
+import org.jetbrains.anko.AnkoComponent
+import org.jetbrains.anko.AnkoContext
+import org.jetbrains.anko.backgroundResource
+import org.jetbrains.anko.button
+import org.jetbrains.anko.dip
+import org.jetbrains.anko.imageView
+import org.jetbrains.anko.linearLayout
+import org.jetbrains.anko.matchParent
+import org.jetbrains.anko.padding
+import org.jetbrains.anko.scrollView
 import org.jetbrains.anko.support.v4.browse
+import org.jetbrains.anko.textView
+import org.jetbrains.anko.verticalLayout
+import org.jetbrains.anko.wrapContent
 import us.feras.mdv.MarkdownView
 
 /**
@@ -42,7 +56,7 @@ class FragmentViewAbout : Fragment(), ContentAPI {
         ui.requinardLayout.setOnClickListener { browse("https://furry.requinard.nl") }
         ui.pazuzuLayout.setOnClickListener { browse("https://twitter.com/Pazuzupizza") }
 
-        ui.helpButton.setOnClickListener { browse("https://t.me/joinchat/AHbDIgZX0NYGill2_ikXXA") }
+        ui.helpButton.setOnClickListener { browse(RemotePreferences.supportChatUrl) }
         ui.bugButton.setOnClickListener { browse("https://github.com/eurofurence/ef-app_android/issues") }
     }
 }
@@ -59,48 +73,87 @@ class AboutUi : AnkoComponent<ViewGroup> {
     override fun createView(ui: AnkoContext<ViewGroup>) = with(ui) {
         scrollView {
             lparams(matchParent, matchParent)
+            backgroundResource = R.color.backgroundGrey
 
             verticalLayout {
-                themedTextView("Developed by", R.style.AppTheme_Header) {
-                    setTextAppearance(ctx, android.R.style.TextAppearance_Large_Inverse)
+
+                linearLayout {
+                    backgroundResource = R.color.cardview_light_background
+                    padding = dip(20)
+                    weightSum = 10f
+
+                    verticalLayout {
+                        textView("Version") {
+                            compatAppearance = android.R.style.TextAppearance_Small
+                        }
+                        textView(BuildConfig.VERSION_NAME) {
+                            compatAppearance = android.R.style.TextAppearance_Medium
+                        }
+                    }.lparams(dip(0), wrapContent) {
+                        weight = 3F
+                    }
+
+                    verticalLayout {
+                        textView("Version Code: ${BuildConfig.VERSION_CODE}") {
+                            compatAppearance = android.R.style.TextAppearance_Small
+                        }
+                        textView("Remote Configuration: ${(now().millis -  RemotePreferences.lastUpdatedDatetime.millis) / 1000 / 60 / 60} hours     ago") {
+                            compatAppearance = android.R.style.TextAppearance_Small
+                        }
+                    }.lparams(dip(0), wrapContent) {
+                        weight = 7F
+                    }
+                }.lparams(matchParent, wrapContent) {
+                    topMargin = dip(20)
                 }
 
-                requinardLayout = linearLayout {
+                verticalLayout {
+                    backgroundResource = R.color.primaryDark
+                    padding = dip(20)
+
+                    textView("Android Developers") {
+                        compatAppearance = android.R.style.TextAppearance_Small_Inverse
+                    }
+
+                    requinardLayout = linearLayout {
+                        setPadding(0, dip(10), 0, 0)
+                        imageView {
+                            imageService.imageLoader.displayImage("https://en.gravatar.com/avatar/42d336e4b6f13d687c32eaaf9c8fb0ea?s=$avatarSize", this)
+                        }.lparams(dip(75), dip(75))
+
+                        textView {
+                            gravity = Gravity.CENTER_VERTICAL
+                            text = "Requinard"
+                            compatAppearance = android.R.style.TextAppearance_Large_Inverse
+                            padding = dip(10)
+                        }.lparams(matchParent, dip(75))
+                    }.lparams(matchParent, wrapContent)
+
+                    pazuzuLayout = linearLayout {
+                        setPadding(0, dip(10), 0, 0)
+                        imageView {
+                            imageService.imageLoader.displayImage("https://en.gravatar.com/avatar/a5db6ad5350a2ee91408120b94d9fa24?s=$avatarSize", this)
+                        }.lparams(dip(75), dip(75))
+
+                        textView {
+                            gravity = Gravity.CENTER_VERTICAL
+                            text = "Pazuzu"
+                            compatAppearance = android.R.style.TextAppearance_Large_Inverse
+                            padding = dip(10)
+                        }.lparams(matchParent, dip(75))
+                    }
+                }.lparams(matchParent, wrapContent) {
+                    topMargin = dip(20)
+                }
+
+                verticalLayout {
+                    padding = dip(20)
                     backgroundResource = R.color.cardview_light_background
 
-                    imageView {
-                        imageService.imageLoader.displayImage("https://en.gravatar.com/avatar/42d336e4b6f13d687c32eaaf9c8fb0ea?s=$avatarSize", this)
-                    }.lparams(dip(avatarSize), dip(avatarSize))
+                    markdownView {
+                        lparams(matchParent, wrapContent)
 
-                    textView {
-                        gravity = Gravity.CENTER
-                        text = "Requinard"
-                        setTextAppearance(ctx, android.R.style.TextAppearance_Medium)
-                    }.lparams(matchParent, dip(avatarSize))
-                }
-
-                pazuzuLayout = linearLayout {
-                    backgroundResource = R.color.cardview_light_background
-                    //setOnTouchListener { _, _ -> browse("https://twitter.com/Pazuzupizza") }
-
-                    imageView {
-                        imageService.imageLoader.displayImage("https://en.gravatar.com/avatar/a5db6ad5350a2ee91408120b94d9fa24?s=$avatarSize", this)
-                    }.lparams(dip(avatarSize), dip(avatarSize))
-
-                    textView {
-                        gravity = Gravity.CENTER
-                        text = "Pazuzu"
-                        setTextAppearance(ctx, android.R.style.TextAppearance_Medium)
-                    }.lparams(matchParent, dip(avatarSize))
-                }
-
-                themedTextView("Version: ${BuildConfig.VERSION_NAME} - Remote: ${RemotePreferences.timeSinceLastUpdate.millis / 1000} seconds old", R.style.AppTheme_Header_Sub)
-
-
-                markdownView {
-                    lparams(matchParent, wrapContent)
-
-                    loadMarkdown("""## Also thanks to
+                        loadMarkdown("""## Also thanks to
 **iOS:**
 
 - Fenrikur
@@ -136,11 +189,23 @@ class AboutUi : AnkoComponent<ViewGroup> {
 - Kotlin
 - Anko
                     """)
+                    }
+                }.lparams(matchParent, wrapContent) {
+                    topMargin = dip(20)
                 }
 
+                linearLayout {
+                    padding = dip(20)
+                    weightSum = 3F
 
-                helpButton = button("Get help in the support telegram") { lparams(matchParent, wrapContent) }
-                bugButton = button("Report bugs on github") { lparams(matchParent, wrapContent) }
+
+                    helpButton = button("Get help").lparams(dip(0), wrapContent) {
+                        weight = 1F
+                    }
+                    bugButton = button("Report a bug").lparams(dip(0), wrapContent) {
+                        weight = 2F
+                    }
+                }
             }
         }
     }
