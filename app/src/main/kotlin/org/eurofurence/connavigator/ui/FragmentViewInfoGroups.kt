16bebe3b1a1fa73059386c58e8c9a495f64fd8cf
requinard <d.diks94@gmail.com>
2018-08-13 11:53:10 +0200

    fix(info): fix faulty layouts

--- app/src/main/kotlin/org/eurofurence/connavigator/ui/FragmentViewInfoGroups.kt
+++ app/src/main/kotlin/org/eurofurence/connavigator/ui/FragmentViewInfoGroups.kt
@@ -66,8 +66,11 @@ class FragmentViewInfoGroups : Fragment(), ContentAPI, HasDb {
 class ViewInfoGroupsUi : AnkoComponent<Fragment> {
     override fun createView(ui: AnkoContext<Fragment>) = with(ui) {
         scrollView {
+            lparams(matchParent, matchParent)
             relativeLayout {
+                lparams(matchParent, wrapContent)
                 verticalLayout {
+                    lparams(matchParent, wrapContent)
                     topPadding = dip(10)
                     id = 1
                 }
