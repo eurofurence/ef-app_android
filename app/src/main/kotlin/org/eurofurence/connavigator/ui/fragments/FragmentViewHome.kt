964019b0b100ce2dfab5652e31f8f4c904513623
requinard <david@requinard.nl>
2019-06-06 16:11:32 +0200

    Revert "Replacing last years banner with placeholder for 2019"
    This reverts commit 17d14b0508cb32760b5452a24bf027118643b6ab.

--- app/src/main/kotlin/org/eurofurence/connavigator/ui/fragments/FragmentViewHome.kt
+++ app/src/main/kotlin/org/eurofurence/connavigator/ui/fragments/FragmentViewHome.kt
@@ -130,7 +130,7 @@ class HomeUi : AnkoComponent<Fragment> {
 
                 lparams(matchParent, matchParent)
 
-                imageView(R.drawable.banner_2019) {
+                imageView(R.drawable.banner_2018) {
                     adjustViewBounds = true
                     setBackgroundColor(Color.WHITE)
                     ViewCompat.setElevation(this, 15f)
